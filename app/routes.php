5b4eb1ad257d54ac23eca3e892cb0e2ea2f18d82
Michał Pałys <michael.palys@wp.pl>
2020-08-05 22:29:29 +0200

    add default value empty array for $parameters

--- app/routes.php
+++ app/routes.php
@@ -6,7 +6,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 return function (App $app) {
-    $app->get('/home', function (Request $request, Response $response, $parameters) {
+    $app->get('/home', function (Request $request, Response $response, $parameters = []) {
         $name = 'Clean Code';
 
         return view($response, 'auth.home', compact('name'));

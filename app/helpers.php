1a25b15095755f4e100ae9b031b430819842ec37
Michał Pałys <michael.palys@wp.pl>
2020-08-12 22:01:30 +0200

    add more helper functions in helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -17,6 +17,62 @@ if (!function_exists('base_path'))
     }
 }
 
+if (!function_exists('database_path'))
+{
+    function database_path($path = '')
+    {
+        return base_path("database/{$path}");
+    }
+}
+
+if (!function_exists('config_path'))
+{
+    function config_path($path = '')
+    {
+        return base_path("config/{$path}");
+    }
+}
+
+if (!function_exists('storage_path'))
+{
+    function storage_path($path = '')
+    {
+        return base_path("storage/{$path}");
+    }
+}
+
+if (!function_exists('public_path'))
+{
+    function public_path($path = '')
+    {
+        return base_path("public_path/{$path}");
+    }
+}
+
+if (!function_exists('resources_path'))
+{
+    function resources_path($path = '')
+    {
+        return base_path("resources/{$path}");
+    }
+}
+
+if (!function_exists('routes_path'))
+{
+    function routes_path($path = '')
+    {
+        return base_path("routes/{$path}");
+    }
+}
+
+if (!function_exists('app_path'))
+{
+    function app_path($path = '')
+    {
+        return base_path("app/{$path}");
+    }
+}
+
 if (!function_exists("view"))
 {
     function view(Response $response, $template, $withData = []) {
